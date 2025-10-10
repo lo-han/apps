5e916d6dec477cc5d81ca2a285561a922a59ddcd
Lo Han <lo.sa@dock.tech>
2025-10-10 18:08:04 -0300

    Add files via upload

---
+++ quick_install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Brave
+sudo snap install brave
+#######################################################
+
+# Code
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+#######################################################
+
+# Containers
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get -y remove $pkg; done
+sudo apt-get -y update
+sudo apt-get -t install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -t update
+
+sudo apt-get -t install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+sudo snap install kubectl --classic
+#######################################################
+
+# Git
+sudo apt -y install git
+#######################################################
+
+# Code Editor synced to GitHub account
+sudo snap install --classic code
+#######################################################
