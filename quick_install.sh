9dd8998b322c67f18ce3fbce7ceecb2154971d92
Lo Han <lohan.chuan123@gmail.com>
2025-10-14 22:30:18 -0300

    Chore: Removed redundant brave app

--- quick_install.sh
+++ quick_install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Brave
-sudo snap install brave
-#######################################################
-
 # Code
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.25.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
