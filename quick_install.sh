8a315aba4d60ce6b1e796408725131149ed34b54
Lo Han <lohan.chuan123@gmail.com>
2025-10-19 12:30:52 -0300

    Add Go download to quick_install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Code
+curl https://go.dev/dl/go1.25.3.linux-amd64.tar.gz > go.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 #######################################################
